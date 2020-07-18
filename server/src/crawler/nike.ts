7f53014f283ba4d7f41f0e0a9bac58b0d9d79bf7
kdsgood12 <kdsgood12@daum.net>
2020-07-18 16:07:23 +0900

    경로

--- server/src/crawler/nike.ts
+++ server/src/crawler/nike.ts
@@ -20,7 +20,7 @@ export default async function crawl() {
 
   //ubuntu server 드라이버 경로
   let service = new chrome.ServiceBuilder(
-    path.join(__dirname, "../../chromedriver.exe"),
+    path.join(__dirname, "../../chromedriver"),
   ).build();
   chrome.setDefaultService(service);
   /* const driver = new webdriver.Builder()
