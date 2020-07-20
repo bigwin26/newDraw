42586e52c61b162b07e32dc93d28e5311a6b4aba
kdsgood12 <kdsgood12@daum.net>
2020-07-20 15:41:55 +0900

    BASE_URL변경

--- server/src/endpoints.config.ts
+++ server/src/endpoints.config.ts
@@ -2,5 +2,5 @@ import { defaults } from "request";
 
 export default {
   MONGO_URL:
-    process.env.MONGO_URL ?? `mongodb://yourdraw:yourkim@13.125.216.20:27017`,
+    process.env.MONGO_URL ?? `mongodb://yourdraw:yourkim@15.165.109.82:27017`,
 };
