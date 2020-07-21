cd17ebcc61d435a2f13268df0f6dabf5b56fcec9
kdsgood12 <kdsgood12@daum.net>
2020-07-21 09:54:50 +0900

    cors URL경로 변경

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ connect();
 crawler();
 //cors
 const corsOptions = {
-  origin: "http://newdrawapp.s3-website.ap-northeast-2.amazonaws.com", // 허락하고자 하는 요청 주소
+  origin: "https://master.d3aavmup2ummck.amplifyapp.com", // 허락하고자 하는 요청 주소
   credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
 };
 app.use(cors(corsOptions));
